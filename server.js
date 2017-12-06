7b6717e11b3fe3caebce305031fa7b561c099f0e
Adrian Piwowarczyk <adrpiw@gmail.com>
2017-12-06 21:38:09 +0000

    Removed express server

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/app'));
-app.use(express.static(__dirname + '/assets'));
-
-var port = process.env.PORT || 8004;
-
-app.listen(port);
